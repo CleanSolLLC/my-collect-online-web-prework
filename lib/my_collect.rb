d3a25f29539bbf760658213612338b1901754296
Mark May <mamark90@gmail.com>
2019-04-04 16:23:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
 
+i = 0
 
+while i < array.length
+  yield (array[i])
+  i+=1
+end
+array
+end
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(languages) {|language| p language.upcase}
