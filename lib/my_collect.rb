0e25a7b6e67384f18b6b6ccdf8d77bca8f8fc1bb
Mark May <mamark90@gmail.com>
2019-04-05 00:01:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,13 +2,13 @@ def my_collect(array)
 i = 0
 new_array = []
 while i < array.length
-  new_array.push yield element_in_array
+  new_array.push yield array[i]
   i+=1
 end
 new_array
 end
 
-languages = ['english', 'spanish', 'french', 'german']
+languages = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(languages) do |language|
   language.upcase
 end
